f1790d303c32b426a57a5a4b8c98781448c5ca50
wucan <wucan@yigucn.com>
2015-12-30 15:00:23 +0800

    add header image add shop info

---
+++ collection.js
@@ -0,0 +1,8 @@
+$(function() {
+    var config = {
+        pagination: '.swiper-pagination',
+        // autoplay: 3000,
+        paginationClickable: true
+    }
+    $(".swiper-container").swiper(config);
+});
