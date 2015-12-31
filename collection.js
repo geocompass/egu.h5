580b436e9f5eed8fa039328b3eb04f79ea603cb9
wucan <wucan@yigucn.com>
2015-12-31 16:05:02 +0800

    添加[藏品详情][藏品鉴定][藏品交易]等，完成主体页面

--- collection.js
+++ collection.js
@@ -1,3 +1,4 @@
+// 顶部图片幻灯片
 $(function() {
     var config = {
         pagination: '.swiper-pagination',
@@ -5,4 +6,23 @@ $(function() {
         paginationClickable: true
     }
     $(".swiper-container").swiper(config);
+});
+
+// 图片浏览器
+$(function() {
+    /*=== 默认为 standalone ===*/
+    var myPhotoBrowserStandalone = $.photoBrowser({
+        photos: [
+            "images/header1.jpg",
+            "images/header2.jpg",
+            "images/header3.jpg",
+            "images/detail1.jpg",
+            "images/detail2.jpg",
+            "images/detail3.jpg"
+        ]
+    });
+    //点击时打开图片浏览器
+    $(document).on('click', '.collection-detail img', function() {
+        myPhotoBrowserStandalone.open();
+    });
 });
